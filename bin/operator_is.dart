adcf8d7bfb6a4d40cabd5f5feffef9b00469a065
afniwar <diway.afniwar@gmail.com>
2022-05-29 09:20:42 +0700

    menambahkan file dart

---
+++ bin/operator_is.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  print(2 is int);
+  print(2 is num);
+  print(2 is String);
+  print([1, 2, 3] is Map);
+}
---
+++ bin/operator_string.dart
@@ -0,0 +1,13 @@
+String reverseString(String s) {
+  String result = "";
+  for (int i = s.length - 1; i >= 0; i--) {
+    result += s[i];
+  }
+  return result;
+}
+
+void main(List<String> args) {
+  String s = 'Pemrograman Android dengan Flutter dan Dart';
+  print(s);
+  print(reverseString(s));
+}
